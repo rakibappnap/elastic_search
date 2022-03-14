d5069bc268ed9841302e343928370a918bcb5e94
rakibappnap <rakibhamid@appnap.io>
2022-03-14 11:32:58 +0600

    :shirt: check case sensitivity in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\ThemeController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ThemeController;
 
 /*
 |--------------------------------------------------------------------------
