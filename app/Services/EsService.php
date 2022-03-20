6ab7a6ad97f3320e0560f175ea9431bdaade13b4
rakibappnap <rakibhamid@appnap.io>
2022-03-20 15:27:24 +0600

    :hammer: increase caption priority

--- app/Services/EsService.php
+++ app/Services/EsService.php
@@ -83,7 +83,7 @@ class EsService
                         'fields' => [
                             'title^5',
                             'category^5',
-                            'caption',
+                            'caption^3',
                         ],
                         'query' => $keyword,
                         'fuzziness' => 'AUTO',
